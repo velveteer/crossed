020951f668ee35831481373bbe3663f3cd8e1c1f
Josh Miller <notjoshmiller@gmail.com>
2016-05-18 05:04:10 +0000

    Styles, more COLORS, new test puzzle

--- src/app/colors.cljs
+++ src/app/colors.cljs
@@ -6,7 +6,13 @@
      
 (def classic (mc "#000000""#FFFFFF"))
 (def toxic (mc "#E2FF00""#6800B2"))
+(def beach-day (mc "#0BFFF6" "#FF610B"))
+(def far-away (mc "#FFB184" "#B26639"))
+(def become-ocean (mc "#6AFFFF" "#27B2B2"))
 
 (def colors
     {:classic classic
+     :beach-day beach-day
+     :far-away far-away
+     :become-ocean become-ocean
      :toxic toxic})
