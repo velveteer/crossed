acf617a8f4c735eadffd0a6c8a1588e8f7e1ad0b
Josh Miller <notjoshmiller@gmail.com>
2016-05-18 07:16:47 +0000

    Check for correct words -- could be fun

--- src/app/colors.cljs
+++ src/app/colors.cljs
@@ -3,20 +3,16 @@
 (defn- mc [color bg]
     {:color                 color
      :background-color      bg})
-     
-(def classic (mc "#000000""#FFFFFF"))
-(def toxic (mc "#E2FF00""#6800B2"))
-(def beach-day (mc "#0BFFF6" "#FF610B"))
-(def far-away (mc "#FFB184" "#B26639"))
-(def become-ocean (mc "#6AFFFF" "#27B2B2"))
-(def bubblegum (mc "#950FB2" "#FF6A90"))
-(def dune (mc "#AAFFFF" "#CCA741"))
 
 (def colors
-    {:classic classic
-     :dune dune
-     :beach-day beach-day
-     :far-away far-away
-     :become-ocean become-ocean
-     :bubblegum bubblegum
-     :toxic toxic})
+    {:classic (mc "#000000""#FFFFFF")
+     :dune (mc "#AAFFFF" "#CCA741")
+     :beach-day (mc "#0BFFF6" "#FF610B")
+     :far-away (mc "#FFB184" "#B26639")
+     :become-ocean (mc "#6AFFFF" "#27B2B2")
+     :bubblegum (mc "#950FB2" "#FF6A90")
+     :mig-28 (mc "#FFFFFF" "#000000")
+     :jungle-juice (mc "#C5E31D" "#8C9942")
+     :toxic (mc "#E2FF00""#6800B2")
+     :you-did-what (mc "#5FFF47" "#FF0735")
+     })
