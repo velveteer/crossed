fb2145dc12cdb45cbf4033995d265f1abfb5710c
Josh Miller <notjoshmiller@gmail.com>
2016-08-21 14:43:54 -0700

    Add favicon

--- src/app/server/core.clj
+++ src/app/server/core.clj
@@ -23,6 +23,8 @@
      [:meta {:charset "utf-8"}]
      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+     [:link {:rel "shortcut icon" :href "/images/favicon.ico" :type "image/x-icon"}]
+     [:link {:rel "icon" :href "/images/favicon.ico" :type "image/x-icon"}]
      (include-css "https://s3-us-west-1.amazonaws.com/tachyons-css/tachyons.min.css")
      (include-css "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css")
      (include-css "/styles/app.css")]
